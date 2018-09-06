a3e971fcf8464b7ec8f63f34c10fb908e2112cb3
Stefanie Schreiner <s.schreiner@qaware.de>
2018-09-06 16:29:20 +0200

    change uri for requests

--- microservices/dashboard-service/src/main/java/de/qaware/oss/cloud/service/dashboard/ProcessServiceClient.java
+++ microservices/dashboard-service/src/main/java/de/qaware/oss/cloud/service/dashboard/ProcessServiceClient.java
@@ -2,6 +2,7 @@ package de.qaware.oss.cloud.service.dashboard;
 
 import io.opentracing.contrib.cdi.Traced;
 import io.opentracing.contrib.jaxrs2.client.ClientTracingFeature;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 
 import javax.annotation.PostConstruct;
@@ -22,6 +23,7 @@ import java.util.concurrent.TimeUnit;
 public class ProcessServiceClient {
 
     private Client client;
+    private Client authClient;
     private WebTarget processService;
 
     @PostConstruct
@@ -31,6 +33,12 @@ public class ProcessServiceClient {
                 .connectTimeout(5, TimeUnit.SECONDS)
                 .readTimeout(5, TimeUnit.SECONDS)
                 .build();
+
+        /*authClient = ClientBuilder.newBuilder()
+                .register(ClientTracingFeature.class)
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .build();*/
 
         processService = client.target("http://gravitee-gateway.local:8080").path("/process-service/api/process");
     }
@@ -43,10 +51,15 @@ public class ProcessServiceClient {
     @Timeout(value = 5, unit = ChronoUnit.SECONDS)
     @Traced
     public void send(String processId, String name, Long amount, String bearer) {
+
+        /*WebTarget userInfo = authClient.target("keycloak-http.security:80").path("/auth/realms/showcase/protocol/openid-connect/userinfo");
+        Response userInfoResponse = userInfo.request().header("Authorization", bearer).get();
+*/
         JsonObject payload = Json.createObjectBuilder()
                 .add("processId", processId)
                 .add("name", name)
                 .add("amount", amount)
+//                .add("user", userInfoResponse.getEntity().toString())
                 .build();
 
         Response response = processService.request().header("Authorization", bearer).post(Entity.json(payload));
